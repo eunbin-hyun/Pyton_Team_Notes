04ef6df9d29555b91c4de0ed5573faefc672ef60
eunbin-hyun <eunbin8592@gmail.com>
2025-07-05 04:15:29 +0000

    이진탐색

---
+++ 이진탐색/이진탐색_반복.py
@@ -0,0 +1,12 @@
+# 이진탐색 반복
+
+def binary_search(arr, target, low, high):
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return None
---
+++ 이진탐색/이진탐색_재귀.py
@@ -0,0 +1,12 @@
+#이진탐색 재귀
+
+def binary_search(arr, target, low, high):
+    if low > high:
+        return -1
+    mid = (low + high) // 2
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return binary_search(arr, target, mid + 1, high)
+    else:
+        return binary_search(arr, target, low, mid - 1)
